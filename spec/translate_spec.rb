8d3ac65254eda5911cfc0470b902933cbb8b3f92
Clay Ash <claycourtneyash@gmail.com>
2022-04-23 17:37:02 -0600

    Test: change expected for transpose

---
+++ spec/translate_spec.rb
@@ -0,0 +1,24 @@
+require './lib/writer'
+require './lib/translate'
+
+RSpec.describe Translate do
+  it 'exists' do
+    translate = Translate.new
+    expect(translate).to be_a(Translate)
+  end
+
+  it 'can translate english char to braille' do
+    translate = Translate.new
+    expect(translate.char_to_braille("h")).to eq(["O.", "OO", ".."])
+  end
+
+  it 'can translate english string to braille' do
+    translate = Translate.new
+    expect(translate.english_to_braille("he")).to eq([["O.", "OO", ".."], ["O.", ".O", ".."]])
+  end
+
+  it 'can transpose braille' do
+    translate = Translate.new
+    expect(translate.transpose_braille([["O.", "OO", ".."], ["O.", ".O", ".."]])).to eq("O.O.\nOO.O\n....")
+  end
+end
